0d09530ce7d12211b62ef430b5f8edb96c69fb3e
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2023-09-24 20:01:09 +0600

    Job data loded wih layout

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -3,6 +3,7 @@ import Jobs from "../Jobs/Jobs";
 
 const FeaturedJob = () => {
   const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     fetch("jobs.json")
@@ -18,11 +19,17 @@ const FeaturedJob = () => {
           need. Its your future
         </p>
       </div>
-      <div>
-        {jobs.map((job) => (
+      <div className="grid grid-cols-2 gap-4">
+        {jobs.slice(0, dataLength).map((job) => (
           <Jobs job={job} key={jobs.id}></Jobs>
         ))}
       </div>
+      <button
+        onClick={() => setDataLength(jobs.length)}
+        className={`btn btn-primary ${dataLength === jobs.length && "hidden"}`}
+      >
+        See All Jobs
+      </button>
     </>
   );
 };
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,8 +1,48 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollar } from "react-icons/ai";
+
 const Jobs = ({ job }) => {
-  const { logo } = job;
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+  } = job;
   return (
     <div>
-      <img src={logo} alt="" />
+      <div className="card bg-base-100 shadow-xl">
+        <div className=" flex justify-start ml-8 ">
+          <figure>
+            <img src={logo} alt="" />
+          </figure>
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">{job_title}</h2>
+          <p>{company_name}</p>
+          <div className="flex">
+            <button className="mr-4 font-extrabold border  border-[#7E90FE] text-[#9873FF] rounded p-2">
+              {remote_or_onsite}
+            </button>
+            <button className="mr-4 font-extrabold border  border-[#7E90FE] text-[#9873FF] rounded p-2">
+              {job_type}
+            </button>
+          </div>
+          <div className="flex">
+            <p className="flex">
+              <MdLocationOn className="text-2xl"></MdLocationOn> {location}
+            </p>
+            <p className="flex">
+              <AiOutlineDollar className="text-2xl"></AiOutlineDollar> {salary}
+            </p>
+          </div>
+          <div className="card-actions ">
+            <button className="btn btn-primary">View Details</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
